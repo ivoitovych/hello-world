efe9f088cb6fef690298ac5b33db82a403bdaf14
Iaroslav Voitovych <ivoitovych@openmobileww.com>
2016-03-09 11:00:35 +0200

    Eliminating magic numbers

--- concurrency_experiment.go
+++ concurrency_experiment.go
@@ -12,9 +12,10 @@ import (
 func dataSource(dataChannel chan float64, sourceNum int) {
 
 	const outputMin, outputMax float64 = -100, 100
+	const delayMaxMs int32 = 1000;
 
 	for {
-		delay := time.Millisecond * time.Duration(rand.Int31n(1000))
+		delay := time.Millisecond * time.Duration(rand.Int31n(delayMaxMs))
 		time.Sleep(delay)
 		number := outputMin + (outputMax - outputMin) * rand.Float64()
 		fmt.Printf("Source %v puts a number %v into the data channel after a delay of %v\n", sourceNum, number, delay);
@@ -24,11 +25,13 @@ func dataSource(dataChannel chan float64, sourceNum int) {
 
 func main() {
 
+	const inputChannelsNumber int = 6
+
     fmt.Println("Hi! Let's the concurrency experiment begin...")
 
 	inputData := make(chan float64);
 	
-	for i := 1; i<6; i++ {
+	for i := 0; i < inputChannelsNumber; i++ {
 		go dataSource(inputData, i)
 	}
 
